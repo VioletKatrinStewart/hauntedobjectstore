d0125532a4f0331946d3bed91e00226509ab680a
Violet Stewart <vkazoo2@gmail.com>
2021-10-11 15:27:35 -0700

    started to figure out my loop

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 import { hauntedStuff } from "./hauntedStuff.js";
+import { renderHaunted } from "./render-function.js";
 
+const productList = document.getElementById('product-list');
+
+for (let spookyScaryStuff of hauntedStuff) {
+  const spookyCard = renderHaunted(spookyScaryStuff);
+  productList.append(spookyCard);
+
+}
