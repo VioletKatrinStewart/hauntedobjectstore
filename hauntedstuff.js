bc718199c5938f24c7e750704c4f15f03e16853a
Violet Stewart <vkazoo2@gmail.com>
2021-10-11 13:56:16 -0700

    added some data modelling

---
+++ hauntedstuff.js
@@ -0,0 +1,22 @@
+export const hauntedStuff = [
+    {
+        id: '1',
+        name: 'Bear',
+        img: './assets/bear.png',
+        rating: '7',
+        price: '$250',
+        origin: 'France, 1935',
+        description: 'This bear belonged to a little girl who died in her sleep of an uknown illness. It is suspected that the paranormal was partially responsable for her falling ill. The bear, origionally kept with her body has been removed just for our website. There is a good chance the ghost of this young girl will visit you if you possess this item'
+    },
+
+    {
+        id: '2',
+        name: 'Doll',
+        img: './assets/doll.jpg',
+        rating: '5',
+        price: '$100',
+        origin: 'USA, 1959',
+        description: 'This doll was recovered from a fallout shelter in an abandoned american home in the midwest. Our experts have carefully determined this doll is indeed haunted though little is known about the previous owner. Although this doll is cursed you will see it is only rated at a 5 on our scale making it a perfect item for beginners who are just starting a collection'
+    }
+
+];
