9901469d64a6d68667b8e779ff7ace2ac84f88d6
Violet Stewart <vkazoo2@gmail.com>
2021-10-11 17:58:13 -0700

    eslint one more time

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { renderHaunted } from './render-function.js';
 const productList = document.getElementById('product-list');
 
 for (let spookyScaryStuff of hauntedStuff) {
-  const spookyCard = renderHaunted(spookyScaryStuff);
-  productList.append(spookyCard);
+    const spookyCard = renderHaunted(spookyScaryStuff);
+    productList.append(spookyCard);
 
 }
