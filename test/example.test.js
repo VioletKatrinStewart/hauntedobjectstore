c3c0438708e2fef3b0f8b9124975214c06c3e9dd
Violet Stewart <vkazoo2@gmail.com>
2021-10-13 13:18:11 -0700

    started testing

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { findById, getCart } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -16,3 +17,27 @@ test('time to test a function', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+test('getCart should return the cart if it exisits', (expect)=>{
+    // arrange
+    const fakeCart = [
+        { id: '1', qty: 3 },
+        { id: '3', qty: 4 }
+    ];
+    localStorage.setItem('CART', JSON.stringify(fakeCart));
+    // act
+    const cart = getCart();
+    // assert
+    expect.deepEqual(cart, fakeCart);
+});
+
+test('getCart should return an empty array if the cart does not exist', (expect)=>{
+    // arrange
+    // nothing to arrange bc nothing in localStorage
+
+    // act 
+    const cart = getCart();
+
+    // assert
+    expect.deepEqual(cart, []);
+});
+
---
+++ utils.js
@@ -0,0 +1,30 @@
+export function findById(id, items){
+    for (let item of items)
+        if (item.id === id)
+            return item;
+}  
+export function getCart(){
+    // null
+    const cartString = localStorage.getItem('CART') || '[]';
+    const cart = JSON.parse(cartString);
+    return cart;
+}
+
+
+// getCart
+    // get the cart from local storage
+    // parse it from JSON to javascript
+    // what if the cart doesn't exist yet?!?! -- return an empty array
+
+
+// addItem(id) --> for now it will always increment by 1
+    // call getCart()
+    // use findById to find a matching a cartItem with the id
+    // if cartItem is found, increment qty by 1
+    // else create a new cartItem with id and qty = 1
+    // stringify the cart back to JSON
+    // set the cart to localstorage
+
+
+// clearCart
+// localStorage.removeItem('CART')
