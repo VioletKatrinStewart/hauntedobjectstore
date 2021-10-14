dc786edb72af02552fd053cf2ca787c6a292369e
Violet Stewart <vkazoo2@gmail.com>
2021-10-13 17:11:31 -0700

    cart js

--- cart/cart.js
+++ cart/cart.js
@@ -1,4 +1,27 @@
-export const cart = [
-    { id: '1', qty: 6 },
-    { id: '4', qty: 5 },
-]; 
+//export const cart = [
+    //{ id: '1', qty: 6 },
+    //{ id: '4', qty: 5 },
+//]; 
+
+import { hauntedStuff } from '../data/hauntedstuff.js';
+import { cart } from '../data/cart-data.js';
+import { findById } from '../utils.js';
+
+const tbody = document.getElementById('table-body');
+for (let cartItem of cart){
+    const cartData = findById(cartItem.id, hauntedStuff);
+    console.log('cartItem: ', cartItem);
+}
+
+const tr = document.createElement('tr');
+const tdName = document.createElement('td');
+tdName.textContent = hauntedStuff.name;
+const tdPrice = document.createElement('td');
+tdPrice.textContent = hauntedStuff.price;
+const tdQty = document.createElement('td');
+tdQty.textContent = cartItem.quantity;
+const tdTotal = document.createElement('td');
+tdTotal.textContent = cartItem.quantity * hauntedStuff.price;
+
+tr.append(tdName, tdPrice, tdQty, tdTotal);
+tbody.appendChild(tr);
