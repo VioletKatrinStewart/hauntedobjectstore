af1eea757815ee2926af0edb5766face296ccf2d
Violet Stewart <vkazoo2@gmail.com>
2021-10-14 11:50:00 -0700

    cart data is showing up

--- cart/cart.js
+++ cart/cart.js
@@ -2,16 +2,17 @@
     //{ id: '1', qty: 6 },
     //{ id: '4', qty: 5 },
 //]; 
-import { calculateOrderTotal, findById, toUSD } from '../utils.js';
+import { findById } from '../utils.js';
 import { hauntedStuff } from '../data/hauntedstuff.js';
 import { cart } from '../data/cart-data.js';
-import { renderLine } from '../render-haunted.js';
+import { renderLine } from '../render-line-items.js';
 
 
 const tbody = document.getElementById('table-body');
 for (let cartItem of cart){
     const cartData = findById(cartItem.id, hauntedStuff);
     const tr = renderLine(cartItem, cartData);
+    console.log(cartItem, cartData);
     tbody.appendChild(tr);
     //console.log('cartItem: ', cartItem);
 }
--- render-line-items.js
+++ render-line-items.js
@@ -1,21 +1,21 @@
 export function renderLine(cartItem, prodData) {
 
-const tr = document.createElement('tr');
+    const tr = document.createElement('tr');
 
-const tdNmae = document.createElement('td');
-tdNmae.textContent = prodData.name
+    const tdNmae = document.createElement('td');
+    tdNmae.textContent = prodData.name;
 
-const tdPrice = document.createElement('td');
-tdPrice.textContent = prodData.price;
+    const tdPrice = document.createElement('td');
+    tdPrice.textContent = prodData.price;
 
-const tdQuantity = document.createElement('td');
-tdQuantity.textContent = cartItem.quantity;
+    const tdQuantity = document.createElement('td');
+    tdQuantity.textContent = cartItem.quantity;
 
-const tdTotal = document.createElement('td');
-tdTotal.textContent = (cartItem.quantity * prodData.price).toFixed(2);
+    const tdTotal = document.createElement('td');
+    tdTotal.textContent = (cartItem.quantity * prodData.price).toFixed(2);
 
-tr.append(tdNmae, tdPrice, tdQuantity, tdTotal);
+    tr.append(tdNmae, tdPrice, tdQuantity, tdTotal);
 
-return tr;
+    return tr;
 
-});
+}
