e2146b5192735b849bf11d44a05ed6c85d12cd66
Violet Stewart <vkazoo2@gmail.com>
2021-10-15 09:55:08 -0700

    clear cart

--- utils.js
+++ utils.js
@@ -35,22 +35,7 @@ export function calculateOrderTotal(cart, hauntedStuff) {
     }
     return orderTotal;
 }
-    
 
-// getCart
-    // get the cart from local storage
-    // parse it from JSON to javascript
-    // what if the cart doesn't exist yet?!?! -- return an empty array
-
-
-// addItem(id) --> for now it will always increment by 1
-    // call getCart()
-    // use findById to find a matching a cartItem with the id
-    // if cartItem is found, increment qty by 1
-    // else create a new cartItem with id and qty = 1
-    // stringify the cart back to JSON
-    // set the cart to localstorage
-
-
-// clearCart
-// localStorage.removeItem('CART')
+export function clearCart() {
+    localStorage.removeItem('CART');
+}
