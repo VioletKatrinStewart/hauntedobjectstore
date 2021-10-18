c0ceda48588daf84e319c8ebc0c6ec7410700e15
Violet Stewart <vkazoo2@gmail.com>
2021-10-18 16:14:25 -0700

    admin js eventlistener written

---
+++ admin/admin.js
@@ -0,0 +1,21 @@
+import { getProducts } from '../utils.js';
+
+const form = document.getElementById('product-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    const newHaunted = {
+        id: data.get('id'),
+        name: data.get('name'),
+        image: data.get('image'),
+        rating: data.get('rating'),
+        price: data.get('price'),
+        origin: data.get('origin'),
+        description: data.get('description')
+
+    };
+
+    getProducts(newHaunted);
+
+});
